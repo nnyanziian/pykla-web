d702d39d4bc55d40fe96a7636691dade4d76acc9
Nnyanzi Ian <nnyanziian@gmail.com>
2018-03-31 16:20:43 +0300

    initial front end setup

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    
+    var pykla = new App();
+    pykla.init();
+});
+
+function App(){
+    this.init=function(){
+        console.log("We see you like to see whats under the hood: go to http://www.meetup.com/pykla to learn more about the community");
+    }
+}
