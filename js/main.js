e3e718f2d985f8a087b719f8fa92f068a3cd45eb
Nnyanzi Ian <nnyanziian@gmail.com>
2018-04-02 10:57:57 +0300

    Animation library added, trasform and Images are remaining

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 $(function () {
-    
+
     var pykla = new App();
     pykla.init();
 });
 
-function App(){
-    this.init=function(){
+function App() {
+    var thisApp = this;
+    this.init = function () {
         console.log("We see you like to see whats under the hood: go to http://www.meetup.com/pykla to learn more about the community");
+        thisApp.clAOS();
+    }
+    this.clAOS = function () {
+        AOS.init({
+            offset: 150,
+            duration: 500,
+            easing: 'ease-in-sine',
+            delay: 100,
+            once: false,
+            disable: 'mobile'
+        });
     }
 }
